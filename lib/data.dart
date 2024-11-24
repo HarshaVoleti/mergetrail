9c5f3e6565db1b25bfc3b79962994c308526acbb
Harshavoleti <mailharshavoleti@gmail.com>
2024-11-25 01:17:13 +0530

    profile

--- lib/data.dart
+++ lib/data.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:testingmerge/homepages/profile.dart';
 
-List navitems = [];
+List navitems = [
+  {
+    'icon': Icons.person,
+    'title': 'Person',
+    'index': 1,
+  },
+];
 
-List pages = [];
+List pages = [
+  Profile(),
+];
